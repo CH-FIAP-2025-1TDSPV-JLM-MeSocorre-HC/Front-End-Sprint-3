4e1eaad7b63a055c260f2f0eb8a3b6bdecf90d5c
matahi-al-said <johnnydiasmathiasjunior@gmail.com>
2025-09-15 15:37:25 -0300

    feat: nav style reworked

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,32 +4,32 @@ export function Layout() {
     return (
         <div className="min-h-screen bg-white-500">
             
-            <nav className="bg-gradient-to-r from-teal-500 to-sky-700">
+            <nav className="bg-[#f5f5f5]">
                 <div className="flex justify-center items-center p-4 gap-x-10">
                     
-                    <Link to="/" className="text-white bg-blue-100/30 p-2 rounded-xl hover:text-gray-100">
+                    <Link to="/" className="text-gray-600 p-2 rounded-xl hover:text-gray-800 hover:bg-blue-100/30">
                         Home
                     </Link>
 
-                    <Link to="/agendamentos" className="text-white bg-blue-200/30 p-2 rounded-xl hover:text-gray-100">
+                    <Link to="/agendamentos" className="text-gray-600 p-2 rounded-xl hover:text-gray-800 hover:bg-blue-100/30">
                         Agendamentos
                     </Link>
-                    <Link to="/notificacoes" className="text-white bg-blue-300/30 p-2 rounded-xl hover:text-gray-100">
+                    <Link to="/notificacoes" className="text-gray-600 p-2 rounded-xl hover:text-gray-800 hover:bg-blue-100/30">
                         Notificações
                     </Link>
-                    <Link to="/ajuda" className="text-white bg-blue-400/30 p-2 rounded-xl hover:text-gray-100">
+                    <Link to="/ajuda" className="text-gray-600 p-2 rounded-xl hover:text-gray-800 hover:bg-blue-100/30">
                         Ajuda
                     </Link>
-                    <Link to="/faq" className="text-white bg-blue-500/30 p-2 rounded-xl hover:text-gray-100">
+                    <Link to="/faq" className="text-gray-600 p-2 rounded-xl hover:text-gray-800 hover:bg-blue-100/30">
                         Perguntas Frequentes
                     </Link>
-                    <Link to="/quem-somos" className="text-white bg-blue-600/30 p-2 rounded-xl hover:text-gray-100">
+                    <Link to="/quem-somos" className="text-gray-600 p-2 rounded-xl hover:text-gray-800 hover:bg-blue-100/30">
                         Quem Somos
                     </Link>
 
                 </div>
             </nav>
-            <main>
+            <main className="flex-1 bg-white p-8">
                 <Outlet/>
             </main>
         </div>
