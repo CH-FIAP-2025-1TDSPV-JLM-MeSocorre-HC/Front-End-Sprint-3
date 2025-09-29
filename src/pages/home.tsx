b52a673d66c85841e26e46f8a311584dbd03d647
luisa-ganasevici <lganasevici@gmail.com>
2025-09-29 15:36:38 -0300

    feat: My Appointments dinamized with types and reusable card components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,22 @@
+import { AgendamentosCard } from "../components/agendamentos-card";
+
 export function Home() {
     return (
         <>
             <h2 className="titulo">
                 Meus Agendamentos
             </h2>
-            <div className="flex bg-gray-100 rounded-xl min-h-20">
-                <div className="basis-2xl rounded-s-xl p-4">
-                    <h4 className="text-[#0077C8] font-bold text-lg">Fisioterapia</h4>
-                    <p className="text-gray-600"><span className="font-bold">Endereço: </span>Hospital das Clínicas FMUSP - R. Dr. Ovídio Pires de Campos, 225 - Cerqueira César, São Paulo - SP, 05403-010</p>
-                </div>
-                <div className="bg-blue-100 p-4 basis-sm rounded-xl flex items-center justify-center">
-                    <p>Segunda-feira, 06/10/2025</p>
-                </div>
-            </div>
-
-                    
-                   
+            <AgendamentosCard
+            id="1"
+            titulo="Fisioterapia"
+            end="Hospital das Clínicas FMUSP: R. Dr. Ovídio Pires de Campos, 225 - Cerqueira César, São Paulo - SP, 05403-010"
+            data="Segunda-feira, 06/10/2025"
+            />
+            <AgendamentosCard
+            id="2"
+            titulo="Radioterapia"
+            end="Hospital das Clínicas FMUSP: R. Dr. Ovídio Pires de Campos, 225 - Cerqueira César, São Paulo - SP, 05403-010"
+            data="Terça-feira, 07/10/2025" />
         </>
     )
 }
---
+++ src/types/agendamentos-props.ts
@@ -0,0 +1,6 @@
+export interface AgendamentosProps {
+    id: string,
+    titulo: string,
+    end: string,
+    data: string
+}
