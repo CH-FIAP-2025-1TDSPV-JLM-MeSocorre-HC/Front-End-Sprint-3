8b6f01079fe0637b31578cb19e9bc4f7021c35b4
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-09-30 17:13:39 -0300

    style: Integrants page updated with students class

--- src/components/aluno.tsx
+++ src/components/aluno.tsx
@@ -3,22 +3,22 @@ import type { AlunoProps } from "../types/aluno-props";
 
 export function Aluno({ nome, img, rm, github, linkedin }: AlunoProps) {
     return (
-        <div className="flex place-content-center mt-6">
-            <div className="aluno">
-                <h4>{nome}</h4>
-                <div>
-                    <img src={img} alt={`Integrante ${nome}`}
-                        className="block mx-auto m-1 mb-4 w-32 h-32 rounded-full border-3 border-solid object-cover" />
-                    <p>RM: {rm}</p>
-                </div>
-                <div className="flex gap-4 mt-2 place-content-center mt-4">
-                    <a href={github} target="_blank" rel="noopener noreferrer">
-                        <FaGithub size={24} />
-                    </a>
-                    <a href={linkedin} target="_blank" rel="noopener noreferrer">
-                        <FaLinkedin size={24} />
-                    </a>
-                </div>
+        <div className="mt-6">
+            <h4>{nome}</h4>
+            <div>
+                <img src={img} alt={`Integrante ${nome}`}
+                    className="block mx-auto m-1 mb-4 w-32 h-32
+                                rounded-full border-3 border-solid object-cover" />
+                <p>1TDSPV</p>
+                <p>RM: {rm}</p>
+            </div>
+            <div className="flex gap-4 mt-2 place-content-center mt-4">
+                <a href={github} target="_blank" rel="noopener noreferrer">
+                    <FaGithub size={24} />
+                </a>
+                <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin size={24} />
+                </a>
             </div>
         </div>
     );
