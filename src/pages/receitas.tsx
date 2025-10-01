16fee990e7c18649e8aa3fa5bc635fda31ce0588
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-10-01 14:26:39 -0300

    style: receipts stylized with empty message

--- src/pages/receitas.tsx
+++ src/pages/receitas.tsx
@@ -1,9 +1,12 @@
 export function Receitas() {
     return (
-        <>
+        <div className="flex flex-col h-full">
             <h1 className="titulo">
                 Minhas Receitas
             </h1>
-        </>
+            <div className="flex justify-center items-center basis-72">
+                <p className="text-gray-500">Você não possui receitas recentes.</p>
+            </div>
+        </div>
     )
 }
