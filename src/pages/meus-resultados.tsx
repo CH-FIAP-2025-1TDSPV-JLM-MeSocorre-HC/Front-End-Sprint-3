672e32af693d07b3fe7ff107fe2ec743be89d930
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2025-10-01 14:22:47 -0300

    style: My Results page stylized empty message

--- src/pages/meus-resultados.tsx
+++ src/pages/meus-resultados.tsx
@@ -7,7 +7,7 @@ export function MeusResultados() {
   );
 
   return (
-    <>
+    <div className="flex flex-col h-full">
       <h1 className="titulo">Meus Resultados</h1>
       <div className="flex h-24">
         <div
@@ -34,6 +34,10 @@ export function MeusResultados() {
           <h4>Imagem</h4>
         </div>
       </div>
-    </>
+
+      <div className="flex justify-center items-center basis-64">
+        <p className="text-gray-500">Você não possui resultados recentes.</p>
+      </div>
+    </div>
   );
 }
